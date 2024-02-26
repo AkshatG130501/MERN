e4b3d448d3e4b2a6394f81662587b96cdc69b3fd
Akshat Gupta <guptaakshat370@gmai.com>
2024-02-26 17:04:05 +0530

    feat: UserRoutes Setup

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import {register} from "./controllers/auth.js"
 import authRoutes from "./routes/auth.js"
-
+import userRoutes from "./routes/users.js"
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -42,6 +42,8 @@ const upload = multer({storage});
 app.post("/auth/register", upload.single("picture"),register)
 
 app.use("/auth",authRoutes);
+
+app.use("/users",userRoutes);
 
 /* MONGOOSE SETUP */
 
