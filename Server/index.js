58762e1adebd60dce4c89b8f0f011cebb66d1731
Akshat Gupta <guptaakshat370@gmai.com>
2024-02-27 11:08:49 +0530

    feat: Post Routes Setup

--- Server/index.js
+++ Server/index.js
@@ -11,6 +11,9 @@ import { fileURLToPath } from "url";
 import {register} from "./controllers/auth.js"
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import { verifyToken } from "./middleware/auth.js";
+import {createPost} from "./controllers/posts.js"
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -40,10 +43,13 @@ const upload = multer({storage});
 
 /* Routes */
 app.post("/auth/register", upload.single("picture"),register)
+app.post("/posts",verifyToken,upload.single("picture"),createPost);
 
 app.use("/auth",authRoutes);
 
 app.use("/users",userRoutes);
+
+app.use("/posts",postRoutes);
 
 /* MONGOOSE SETUP */
 
