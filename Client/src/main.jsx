f0be17f9a68a805816e9b5da6d1c028c98ddcdb6
Akshat Gupta <guptaakshat370@gmai.com>
2024-02-27 14:37:12 +0530

    feat: Frontend Routes Setup

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import LoginPage from './pages/loginPage/LoginPage.jsx'
+import HomePage from './pages/homePage/HomePage.jsx'
+import ProfilePage from './pages/profilePage/ProfilePage.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<LoginPage/>} />
+      <Route path='/home' element={<HomePage/>} />
+      <Route path='/profile/:userId' element={<ProfilePage/>} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
