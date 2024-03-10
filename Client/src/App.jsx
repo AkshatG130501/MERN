3fdb7b1fdeee2d74aa9cdd4eaf3d2cb850ae4933
Akshat Gupta <guptaakshat370@gmai.com>
2024-03-10 13:27:49 +0530

    final commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import HomePage from "./pages/homePage/HomePage";
+import ProfilePage from "./pages/profilePage/ProfilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
 import './App.css'
+import { Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import LoginPage from "./pages/loginPage/LoginPage";
+
+
+
 
 function App() {
 
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path='/' element={<LoginPage/>} />
+        <Route path='/home' element={isAuth ? <HomePage/> : <Navigate to='/'/>} />
+        <Route path="/profile/:userId" element={isAuth ? <ProfilePage/> : <Navigate to="/"/>}/>
+      </Route>
+    )
+  )
   return (
     <>
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router}/>
+      </ThemeProvider>
     </>
   )
 }
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,23 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import LoginPage from './pages/loginPage/LoginPage.jsx'
-import HomePage from './pages/homePage/HomePage.jsx'
-import ProfilePage from './pages/profilePage/ProfilePage.jsx'
+import authReducer from "./state/index.js";
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path='/' element={<LoginPage/>} />
-      <Route path='/home' element={<HomePage/>} />
-      <Route path='/profile/:userId' element={<ProfilePage/>} />
-    </Route>
-  )
-)
+const persistConfig = {key:"root", storage, version: 1}
+const persistedReducer = persistReducer(persistConfig,authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 )
